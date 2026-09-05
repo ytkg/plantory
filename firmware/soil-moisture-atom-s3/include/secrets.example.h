69ee207254d1e693222058cfe3340ef420b7d8da
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2026-09-05 17:02:23 +0900

    feat(firmware): add scheduled soil moisture sender

---
+++ firmware/soil-moisture-atom-s3/include/secrets.example.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#define WIFI_SSID "your-wifi-ssid"
+#define WIFI_PASSWORD "your-wifi-password"
+#define PLANT_ID 1
+#define PLANTORY_API_KEY "plnt_your_write_api_key"
+#define SOIL_SENSOR_ANALOG_PIN 1
